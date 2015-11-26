a648dd5357a8beb950d1735ed6c2e9c7919ac79e
Felix Lehmann <felix-lehmann@gmx.de>
2015-11-26 17:22:31 +0100

    First Commit

---
+++ js/javascript.js
@@ -0,0 +1,111 @@
+  
+(function ($) {
+
+    "use strict"
+    // set default times at 25 and 5
+    var $breakLength = 1,
+        $sessionLength = 1,
+        $seconds = $sessionLength*60, 
+        $timeRunning = false,
+        $runTimer,
+        $focus = "session";
+  
+    // set time by clicking arrows
+    function updateBreakLength (e) {
+      var $this = $(this);
+      if ($this.prop('id') == 'breakup') {
+         $breakLength += 1;
+      } else {
+        $breakLength -= 1;
+      }
+      $('.break-duration').text(""+$breakLength);
+    };
+
+    function updateSessionLength (e) {
+      var $this = $(this);
+      if ($this.prop('id') == 'sessionup') {
+         $sessionLength += 1;
+      } else {
+        $sessionLength -= 1;
+      }
+      $seconds = Number($sessionLength*60);
+      $('.session-duration').text(""+$sessionLength);
+      $('p.time').text(setDuration($seconds));
+    };
+
+    function setDuration (time) {
+      var $t =  Number(time);
+      var $h = Math.floor($t / 3600);
+      var $m = Math.floor($t % 3600 / 60);
+      var $s = Math.floor($t % 3600 % 60 );
+      return (
+        ($h > 0 ? $h+ ":" + ($m < 10 ? "0" : "") : "") + $m + ":" + ($s < 10 ? "0" : "") + $s
+        );
+    }
+
+    function clickOnTimer() {
+      if ($timeRunning == false){
+        $timeRunning = true;
+        $runTimer = setInterval(function() {
+          updateTimer();
+        }, 1000);
+        // set timerrunning to true and start the timer
+      } else {
+        clearInterval($runTimer);
+        $timeRunning = false;
+        // set timerRunning to false and stop the timer
+      }
+    }
+
+    function updateTimer() {
+      $seconds -= 1;
+      $('p.time').text(setDuration($seconds)); 
+      if ($seconds == 0){
+       toggleSession();
+       console.log($focus);
+      }   
+    }
+    function toggleSession() {
+      if ($focus == "session") {
+        $focus = "break";
+        $('#circle').css("background-color", "rgb(255,0,0)" );
+        $seconds = $breakLength*60;
+      } else {
+        $focus = "session";
+        $('#circle').css("background-color", "rgb(0,255,0)" );
+        $seconds = $sessionLength*60;
+      }
+    }
+
+    function bindings () {
+        // Increase or decrease timer
+        $('.breakLength i').on('click', updateBreakLength);
+        $('.sessionLength i').on('click', updateSessionLength);
+        $('#circle').on('click', clickOnTimer);
+    }
+
+  $( document ).ready(function() {
+    $('.session-duration').text(""+$sessionLength);
+    $('p.time').text(setDuration(""+$seconds));
+    $('.break-duration').text(""+$breakLength);
+    bindings();
+  });
+
+})(jQuery);
+
+
+
+
+    // get Time from Session Element in seconds
+    // 
+    // start / stop timer by clicking on circle
+    // decrease timer every second by 1
+    // if session timer is 0, start break
+    // if break timer is 0, start next session
+    // 
+    // reset timer when clicking arrows
+    // 
+    // fast forward / "skip break" (supermode)
+    // 
+    // 
+    // 
